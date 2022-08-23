a37d0d4f84f528755c1883affddc32fef41c4947
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-23 23:43:17 +0200

    added cats

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("sounds/sweet.wav");
       tom1.play();
       break;
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+      var tom2 = new Audio("sounds/angry.wav");
       tom2.play();
       break;
     case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
+      var tom3 = new Audio("sounds/cartoon.wav");
       tom3.play();
       break;
     case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
+      var tom4 = new Audio("sounds/purr.wav");
       tom4.play();
       break;
     case "j":
-      var snare = new Audio("sounds/snare.mp3");
+      var snare = new Audio("sounds/slow.wav");
       snare.play();
       break;
     case "k":
-      var crash = new Audio("sounds/crash.mp3");
+      var crash = new Audio("sounds/hungry.wav");
       crash.play();
       break;
     case "l":
-      var kick = new Audio("sounds/kick-bass.mp3");
+      var kick = new Audio("sounds/tired.mp3");
       kick.play();
       break;
 
